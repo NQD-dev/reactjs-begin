1972e3297f4968471fed4256b89f0bd0e6c96350
vinhdm <vinhdm@vnext.com.vn>
2022-02-11 17:02:45 +0900

    duong

--- src/App.js
+++ src/App.js
@@ -1,36 +1,86 @@
 import logo from './logo.svg';
-import './App.css';
 
+import { Table } from 'antd';
 import { useState, useEffect } from "react";
+import 'antd/dist/antd.css';
 
 
 function App() {
-  var [data, setData] = useState();
+  const [global, setGlobal] = useState([]);
+  const [data, setData] = useState();
+  const [dataVN, setDataVN] = useState();
 
-  useEffect( async () => {
-    let xx= await fetch('https://api.covid19api.com/summary')
-        .then(response => response.json());
+  useEffect(async () => {
+    let xx = await fetch('https://api.covid19api.com/summary')
+      .then(response => response.json());
     setData(xx.Countries);
- }, []);
- 
+    setGlobal(xx.Global);
+  }, []);
 
-  return (
-    <>
-    {data && data.map(item => (
-      item.CountryCode == "VN" ? (
-        <li key={item.id}>
-          <h2>Country: {item.Country}</h2>
-          <p>TotalConfirmed: {item.TotalConfirmed}</p>
-          <p>TotalDeaths: {item.TotalDeaths}</p>
-          <p>Date: {item.Date}</p>
-        </li>
-        ) : (
-            <p></p>
-          )
-              
-      ))}
-    </> 
-  );
+  const columns = [
+    {
+      title: 'Country',
+      dataIndex: 'Country'
+    },
+    {
+      title: 'NewConfirmed',
+      dataIndex: 'NewConfirmed'
+    },
+    {
+      title: 'TotalConfirmed',
+      dataIndex: 'TotalConfirmed'
+    },
+    {
+      title: 'NewDeaths',
+      dataIndex: 'NewDeaths'
+    },
+    {
+      title: 'TotalDeaths',
+      dataIndex: 'TotalDeaths'
+    },
+    {
+      title: 'NewRecovered',
+      dataIndex: 'NewRecovered'
+    },
+  ]
+
+  const columns2 = [
+    {
+      title: 'rank',
+      render: () => <p>Global</p>
+    },
+    {
+      title: 'NewConfirmed',
+      dataIndex: 'NewConfirmed'
+    },
+    {
+      title: 'TotalConfirmed',
+      dataIndex: 'TotalConfirmed'
+    },
+    {
+      title: 'NewDeaths',
+      dataIndex: 'NewDeaths'
+    },
+    {
+      title: 'TotalDeaths',
+      dataIndex: 'TotalDeaths'
+    },
+    {
+      title: 'NewRecovered',
+      dataIndex: 'NewRecovered'
+    },
+  ]
+  if (data == undefined) {
+    return (<></>)
+  }
+  else {
+    return (
+      <>
+        <Table key={global.Date} rowKey={global.Date} columns={columns2} dataSource={[global]} />
+        <Table key={data.ID} rowKey={data.ID} columns={columns} dataSource={data} />
+      </>
+    );
+  }
 }
 
 export default App;
